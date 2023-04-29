68af0b4a7e8f0120f69982a12339990e769d3074
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2023-04-29 16:27:17 +0300

    session 12

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div>
+  <h1 class="text-3xl font-bold underline"> {{ name ? name : "Hello PowerHumans" }}</h1>
+  <h2>{{ post.data.title }}</h2>
+</div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'IndexPage',
+  async asyncData({ params }) {
+      const post = await axios.get(`https://api.nuxtjs.dev/posts/1`)
+      return { post }
+    }
+}
+</script>
